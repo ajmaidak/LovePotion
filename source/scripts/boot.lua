6ba5d07e506ba6482bece3849562a621764f79ba
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2020-02-04 13:55:33 -0500

    set up main loop event handlers

--- source/scripts/boot.lua
+++ source/scripts/boot.lua
@@ -223,6 +223,19 @@ function love.run()
     local delta = 0
 
     return function()
+        if love.event then
+            -- love.event.pump()
+
+            -- for name, a, b, c, d, e, f in love.event.poll() do
+            --     if name == "quit" then
+            --         if not love.quit or not love.quit() then
+            --             return a or 0
+            --         end
+            --     end
+            --     love.handlers[name](a, b, c, d, e, f)
+            -- end
+        end
+
         if love.timer then
             delta = love.timer.step()
         end
